4da32a8ff1ea4c8a980980affe47ab165cfb23d6
ZMP <zane.prunella@gmail.com>
2024-02-16 08:12:46 -0800

    Sidebar Started

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectsSideBar from "./components/ProjectsSidebar";
+
+function App() {
+  return (
+    <main>
+      <ProjectsSideBar />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,11 @@
+export default function ProjectsSideBar() {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul>...</ul>
+    </aside>
+  );
+}
