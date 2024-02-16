921e4165a9be195fcf407d542de1678cba31e82f
ZMP <zane.prunella@gmail.com>
2024-02-16 08:56:05 -0800

    Button/Input Tailwind Styling

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,16 @@
 export default function Input({ label, textArea, ...props }) {
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-600 focus:outline-none focus:border-stone-600";
   return (
-    <p>
-      <label>{label}</label>
-      {textArea ? <textarea {...props} /> : <input {...props} />}
+    <p className="flex flex-col  gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
+      {textArea ? (
+        <textarea className={classes} {...props} />
+      ) : (
+        <input className={classes} {...props} />
+      )}
     </p>
   );
 }
