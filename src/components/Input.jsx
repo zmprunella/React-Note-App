ccb27384a36104510f79c2ac1965fc5e04a48d1b
ZMP <zane.prunella@gmail.com>
2024-02-17 09:38:04 -0800

    User input with Refs and Forward

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,6 @@
-export default function Input({ label, textArea, ...props }) {
+import { forwardRef } from "react";
+
+const Input = forwardRef(function Input({ label, textArea, ...props }, ref) {
   const classes =
     "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-600 focus:outline-none focus:border-stone-600";
   return (
@@ -7,10 +9,12 @@ export default function Input({ label, textArea, ...props }) {
         {label}
       </label>
       {textArea ? (
-        <textarea className={classes} {...props} />
+        <textarea ref={ref} className={classes} {...props} />
       ) : (
-        <input className={classes} {...props} />
+        <input ref={ref} className={classes} {...props} />
       )}
     </p>
   );
-}
+});
+
+export default Input;
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,6 +1,22 @@
 import Input from "./Input";
+import { useRef } from "react";
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredueDate = dueDate.current.value;
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredueDate,
+    });
+  }
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -10,15 +26,18 @@ export default function NewProject() {
           </button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
             Save
           </button>
         </li>
       </menu>
       <div>
-        <Input label="Title" />
-        <Input label="Description" textArea />
-        <Input label="Due Date" />
+        <Input type="text" ref={title} label="Title" />
+        <Input ref={description} label="Description" textArea />
+        <Input type="date" ref={dueDate} label="Due Date" />
       </div>
     </div>
   );
