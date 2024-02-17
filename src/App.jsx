41e3329979f63b844888fed73353527266c81e78
ZMP <zane.prunella@gmail.com>
2024-02-16 18:49:14 -0800

    State between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import NewProject from "./components/NewProject";
+import { useState } from "react";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSideBar from "./components/ProjectsSidebar";
+import NewProject from "./components/NewProject";
 
 function App() {
+  const [projectsState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8  flex gap-8">
-      <ProjectsSideBar />
-      <NewProject />
+      <ProjectsSideBar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
